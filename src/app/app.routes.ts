0676587b34fd523541e5af3ee8cbacd738d25643
Christopher Walker <512444+cjw4@users.noreply.github.com>
2025-09-29 09:41:50 +0200

    initial commit, player and event-list added, tailwind installed with basic header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { EventListComponent } from './event-list/event-list.component';
+import { PlayerListComponent } from './player-list/player-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'events',
+        pathMatch: 'full'
+    },
+    {
+        path: 'events',
+        component: EventListComponent
+    },
+    {
+        path: 'players',
+        component: PlayerListComponent
+    }
+];
