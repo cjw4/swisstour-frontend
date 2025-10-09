e4fe80718b9b21117328d4c1a9ccf5d89dc1af80
Christopher Walker <512444+cjw4@users.noreply.github.com>
2025-10-09 21:35:50 +0100

    get all players

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { APP_SETTINGS, appSettings } from '../app.settings';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-player-list',
   imports: [],
   templateUrl: './player-list.component.html',
-  styleUrl: './player-list.component.css'
+  styleUrl: './player-list.component.css',
+  providers: [
+    { provide: APP_SETTINGS, useValue: appSettings }
+  ]
 })
-export class PlayerListComponent {
+export class PlayerListComponent implements OnInit {
+  http = inject(HttpClient);
+  settings = inject(APP_SETTINGS);
+  playersUrl = this.settings.apiUrl + '/players';
+  players: any;
+
+  private getPlayers() {
+    this.http.get(this.playersUrl).subscribe(
+      (result) => {
+        console.log(result);
+        this.players = result;
+      },
+      (error) => {
+        console.error('Error fetching players:', error);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.getPlayers();
+  }
 
 }
