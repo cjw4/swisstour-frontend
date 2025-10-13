36a634f6b8b5de9557b056b4ac1fae876304cc87
Christopher Walker <512444+cjw4@users.noreply.github.com>
2025-10-13 19:51:29 +0100

    further fix to form validation

--- src/app/event-create/event-create.component.ts
+++ src/app/event-create/event-create.component.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, output } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormBuilder, FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { APP_SETTINGS, appSettings } from '../app.settings';
-import { EventListComponent } from '../event-list/event-list.component';
 import { BannerInfo } from '../interfaces/banner-info';
 
 @Component({
@@ -11,19 +10,26 @@ import { BannerInfo } from '../interfaces/banner-info';
   imports: [ReactiveFormsModule],
   templateUrl: './event-create.component.html',
   styleUrl: './event-create.component.css',
-  providers: [
-    { provide: APP_SETTINGS, useValue: appSettings }
-  ]
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{ provide: APP_SETTINGS, useValue: appSettings }],
 })
 export class EventCreateComponent {
-
   eventForm = new FormGroup({
-    id: new FormControl(''),
-    displayName: new FormControl(''),
-    points: new FormControl(''),
+    id: new FormControl('', [Validators.required, Validators.pattern(/^\d+$/)]),
+    displayName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    points: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\d+$/),
+    ]),
     isChampionship: new FormControl(false),
-    isSwisstour: new FormControl(false)
+    isSwisstour: new FormControl(false),
   });
+
+
+  isFormInvalid = this.eventForm.invalid;
 
   http = inject(HttpClient);
   refreshEvents = output();
@@ -32,50 +38,60 @@ export class EventCreateComponent {
   eventUrl = this.settings.apiUrl + '/events';
 
   bannerInfoOutput = output<BannerInfo>();
-  bannerInfo : BannerInfo | undefined;
+  bannerInfo: BannerInfo | undefined;
 
   private emitBannerInfo(bannerInfo: BannerInfo) {
-    debugger;
     this.bannerInfoOutput.emit(bannerInfo);
   }
 
   onSubmit() {
+    if (this.eventForm.invalid) {
+      this.eventForm.markAllAsTouched(); // Mark all fields as touched to show validation errors
+      return;
+    }
     if (this.eventForm.valid) {
-      debugger;
-      this.http.post(this.eventUrl, this.eventForm.value)
-        .subscribe({
-          next: (res: any) => {
-            debugger;
-            this.bannerInfo = {
-              message: `PDGA event ${res.id} (${res.name}) was added.`,
-              visible: true,
-              type: 'success'
-            }
-            this.emitBannerInfo(this.bannerInfo);
-            this.refreshEvents.emit();
-            // this.eventListComponent.getEvents(); // Refresh the event list
-            this.eventForm.reset({
-              id: '',
-              displayName: '',
-              points: '',
-              isChampionship: false,
-              isSwisstour: false
-            }); // Reset the form after submission
-          },
-          error: (err) => {
-            debugger;
-            const serverError = err.error;
-            const errorMessage = serverError?.message || "An unknown error occured."
-            this.bannerInfo = {
-              message: errorMessage,
-              visible: true,
-              type: 'error'
-            }
-            this.emitBannerInfo(this.bannerInfo);
-          }
-        });
+      this.http.post(this.eventUrl, this.eventForm.value).subscribe({
+        next: (res: any) => {
+          this.bannerInfo = {
+            message: `PDGA event ${res.id} (${res.name}) was added.`,
+            visible: true,
+            type: 'success',
+          };
+          this.emitBannerInfo(this.bannerInfo);
+          this.refreshEvents.emit();
+          // this.eventListComponent.getEvents(); // Refresh the event list
+          this.eventForm.reset({
+            id: '',
+            displayName: '',
+            points: '',
+            isChampionship: false,
+            isSwisstour: false,
+          }); // Reset the form after submission
+        },
+        error: (err) => {
+          const serverError = err.error;
+          const errorMessage =
+            serverError?.message || 'An unknown error occured.';
+          this.bannerInfo = {
+            message: errorMessage,
+            visible: true,
+            type: 'error',
+          };
+          this.emitBannerInfo(this.bannerInfo);
+        },
+      });
     }
   }
 
+  get id() {
+    return this.eventForm.get('id');
+  }
 
+  get displayName() {
+    return this.eventForm.get('displayName');
+  }
+
+  get points() {
+    return this.eventForm.get('points');
+  }
 }
