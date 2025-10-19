7a96ccbc529fc99ba1ab6a4879d880356662d915
Christopher Walker <512444+cjw4@users.noreply.github.com>
2025-10-18 19:41:45 -0500

    significant improvement to the standings

--- src/app/standings/standings.component.ts
+++ src/app/standings/standings.component.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, input, OnInit, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { APP_SETTINGS, appSettings } from '../app.settings';
 import { PdgaEvent } from '../interfaces/pdga-event';
 import { EventsService } from '../services/events.service';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { StandingsDTO } from '../interfaces/standings-dto';
+import { PlayerService } from '../services/player.service';
+import { Player } from '../interfaces/player';
 
 @Component({
   selector: 'app-standings',
@@ -15,16 +18,21 @@ import { StandingsDTO } from '../interfaces/standings-dto';
   providers: [{ provide: APP_SETTINGS, useValue: appSettings }],
 })
 export class StandingsComponent implements OnInit {
-  public category: String = 'MPO';
+  category = input();
   private eventsService: EventsService;
+  private playerService: PlayerService;
+  playersSignal: Signal<any>;
 
   constructor() {
     this.eventsService = new EventsService();
+    this.playerService = new PlayerService();
+    this.playersSignal = toSignal(this.playerService.getPlayers())
   }
 
   http = inject(HttpClient);
 
   events$: Observable<PdgaEvent[]> | undefined;
+  players$: Observable<Player[]> | undefined;
   settings = inject(APP_SETTINGS);
   standingsUrl = this.settings.apiUrl + '/standings/' + this.category;
   standings: any;
@@ -39,6 +47,16 @@ export class StandingsComponent implements OnInit {
     this.events$ = this.eventsService.getEvents();
   }
 
+  public getPlayers() {
+    this.players$ = this.playerService.getPlayers();
+  }
+
+  findPlayer(playerId: number) {
+    const players = this.playersSignal();
+    const player = players.find((p: { id: number; }) => p.id === playerId);
+    return player;
+  }
+
   hasEventId(standing: StandingsDTO, eventIdToFind: number): boolean {
     return standing.eventPoints.some(
       (event) => event.eventId === eventIdToFind
@@ -46,13 +64,21 @@ export class StandingsComponent implements OnInit {
   }
 
   getEventPoints(standing: StandingsDTO, eventIdToFind: number): number {
-    const foundEvent = standing.eventPoints.find((event) => event.eventId === eventIdToFind);
+    const foundEvent = standing.eventPoints.find(
+      (event) => event.eventId === eventIdToFind
+    );
     return foundEvent ? foundEvent.points : 0;
   }
 
   ngOnInit(): void {
     this.getStandings();
     this.getEvents();
+    this.getPlayers();
     debugger;
+  }
+
+  onCategoryChange(): void {
+    this.standingsUrl = this.settings.apiUrl + '/standings/' + this.category;
+    this.getStandings();
   }
 }
