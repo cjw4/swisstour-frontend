977fbf29e820c4d93702e36a651cd2af3c2b79d9
Christopher Walker <512444+cjw4@users.noreply.github.com>
2025-11-15 16:27:05 -0500

    hamburger menu fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { APP_SETTINGS, appSettings } from './app.settings';
-import { CopyrightDirective } from './directives/copyright.directive';
 import { BannerComponent } from './banner/banner.component';
 import { LoadingIndicatorComponent } from './loading-indicator/loading-indicator.component';
+import { CopyrightDirective } from './directives/copyright.directive';
+import { APP_SETTINGS, appSettings } from './app.settings';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     RouterLink,
-    CopyrightDirective,
     BannerComponent,
-    LoadingIndicatorComponent
+    LoadingIndicatorComponent,
+    CopyrightDirective,
+    NgOptimizedImage,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  providers: [
-    { provide: APP_SETTINGS, useValue: appSettings }
-  ]
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{ provide: APP_SETTINGS, useValue: appSettings }],
 })
 export class AppComponent {
   settings = inject(APP_SETTINGS);
+  mobileMenuOpen = signal(false);
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen.update((v) => !v);
+  }
 }
