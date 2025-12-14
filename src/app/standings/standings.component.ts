0a7a4e274f663acafa3d15ffa5fb119e44376528
Christopher Walker <512444+cjw4@users.noreply.github.com>
2025-12-14 18:10:09 +0000

    round standings

--- src/app/standings/standings.component.ts
+++ src/app/standings/standings.component.ts
@@ -1,11 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, input, OnInit, Signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { APP_SETTINGS, appSettings } from '../app.settings';
 import { PdgaEvent } from '../interfaces/pdga-event';
 import { EventsService } from '../services/events.service';
-import { filter, map, Observable, tap } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { map, Observable, tap } from 'rxjs';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { StandingsDTO } from '../interfaces/standings-dto';
 import { PlayerService } from '../services/player.service';
 import { Player } from '../interfaces/player';
@@ -14,7 +13,7 @@ import { StandingsService } from '../services/standings.service';
 
 @Component({
   selector: 'app-standings',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, CommonModule],
   templateUrl: './standings.component.html',
   styleUrl: './standings.component.css',
   providers: [{ provide: APP_SETTINGS, useValue: appSettings }],
