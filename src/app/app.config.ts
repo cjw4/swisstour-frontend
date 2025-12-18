645035503823118d1d90df7082098c31820e5e67
Christopher Walker <512444+cjw4@users.noreply.github.com>
2025-12-18 19:02:28 +0000

    fix render problem with SPA redirect

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -9,8 +9,8 @@ import { authInterceptor } from './interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withInterceptors([authInterceptor])),
-    { provide: APP_SETTINGS, useValue: appSettings }
-  ]
+    { provide: APP_SETTINGS, useValue: appSettings },
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,52 +7,58 @@ import { EventInputComponent } from './event-input/event-input.component';
 import { PlayerDetailsComponent } from './player-details/player-details.component';
 import { LoginComponent } from './login/login.component';
 import { authGuard } from './guards/auth.guard';
+import { appSettings } from './app.settings';
 
 export const routes: Routes = [
-    {
-      path:'',
-      redirectTo: 'standings',
-      pathMatch: 'full'
-    },
-    {
-      path: 'events/:year',
-      component: EventListComponent,
-      canActivate: [authGuard]
-    },
-    {
-      path: 'event/input',
-      component: EventInputComponent,
-      canActivate: [authGuard]
-    },
-    {
-      path: 'event/input/:id',
-      component: EventInputComponent,
-      canActivate: [authGuard]
-    },
-    {
-      path: 'players',
-      component: PlayerListComponent
-    },
-    {
-      path: 'player/input',
-      component: PlayerInputComponent,
-      canActivate: [authGuard]
-    },
-    {
-      path: 'player/input/:id',
-      component: PlayerInputComponent,
-      canActivate: [authGuard]
-    },
-    {
-      path: 'player/:id',
-      component: PlayerDetailsComponent
-    },
-    {
-      path: 'standings/:year/:category',
-      component: StandingsComponent
-    },
-    {
-      path: 'admin/login',
-      component: LoginComponent
-    }
+  {
+    path: '',
+    redirectTo: 'standings/' + appSettings.currentYear,
+    pathMatch: 'full',
+  },
+  {
+    path: 'events/:year',
+    component: EventListComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'event/input',
+    component: EventInputComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'event/input/:id',
+    component: EventInputComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'players',
+    component: PlayerListComponent,
+  },
+  {
+    path: 'player/input',
+    component: PlayerInputComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'player/input/:id',
+    component: PlayerInputComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'player/:id',
+    component: PlayerDetailsComponent,
+  },
+  {
+    path: 'standings/:year/:category',
+    component: StandingsComponent,
+  },
+  {
+    path: 'admin/login',
+    component: LoginComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'standings/' + appSettings.currentYear,
+    pathMatch: 'full',
+  },
 ];
