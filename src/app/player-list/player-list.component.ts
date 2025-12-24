a9a57d828080bdfb47d45fa907197e279d44664f
Christopher Walker <512444+cjw4@users.noreply.github.com>
2025-12-24 11:01:32 +0000

    add search for players

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, Signal, signal } from '@angular/core';
 import { APP_SETTINGS, appSettings } from '../app.settings';
 import { filter, map, Observable, switchMap, tap, toArray } from 'rxjs';
 import { Player } from '../interfaces/player';
@@ -16,21 +16,30 @@ import { AuthService } from '../services/auth.service';
   providers: [{ provide: APP_SETTINGS, useValue: appSettings }],
 })
 export class PlayerListComponent implements OnInit {
+  [x: string]: any;
   // inject services
   playerService = inject(PlayerService);
   bannerService = inject(BannerService);
   authService = inject(AuthService);
-  router = inject(Router)
+  router = inject(Router);
 
+  // variables
   players$: Observable<Player[]> | undefined;
   player$: Observable<Player> | undefined;
+  searchTerm = signal('');
+
+  updateSearchTerm(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.searchTerm.set(inputElement.value);
+    this.getPlayers()
+  }
 
   addPlayer() {
-    this.router.navigate(['/player/input'])
+    this.router.navigate(['/player/input']);
   }
 
   editPlayer(id: number) {
-    this.router.navigate(['/player/input', id])
+    this.router.navigate(['/player/input', id]);
   }
 
   getPlayers() {
@@ -44,15 +53,30 @@ export class PlayerListComponent implements OnInit {
             if (b.sdaNumber === null) return -1; // Place nulls at the bottom
             return a.sdaNumber - b.sdaNumber; // Regular ascending sort for non-null values
           })
-        )
+        ),
+        map((players) => this.filterPlayers(players)) // Filter players based on searchTerm
       );
     } else {
       this.players$ = this.playerService.getPlayers().pipe(
         map((arr) => arr.sort((a, b) => a.sdaNumber - b.sdaNumber)), // Sort by 'id' in ascending order
-        map(players => players.filter(player => player.swisstourLicense))
+        map((players) => players.filter((player) => player.swisstourLicense)),
+        map((players) => this.filterPlayers(players)) // Filter players based on searchTerm
       );
     }
+  }
 
+  // Helper method to filter players based on search term
+  private filterPlayers(players: Player[]) {
+    const term = this.searchTerm().toLowerCase();
+    return players.filter(
+      (player) =>
+        player.firstname.toLowerCase().includes(term) ||
+        player.lastname.toLowerCase().includes(term) ||
+        (player.sdaNumber !== null &&
+          player.sdaNumber.toString().includes(term)) ||
+        (player.pdgaNumber !== null &&
+          player.pdgaNumber.toString().includes(term))
+    );
   }
 
   getPlayer(id: number) {
@@ -60,8 +84,12 @@ export class PlayerListComponent implements OnInit {
   }
 
   deletePlayer(player: Player) {
-    if (confirm(`Are you sure you want to delete the player ${player.firstname} ${player.lastname}?`)) {
-      this.players$ = this.playerService.deletePlayer(player)
+    if (
+      confirm(
+        `Are you sure you want to delete the player ${player.firstname} ${player.lastname}?`
+      )
+    ) {
+      this.players$ = this.playerService.deletePlayer(player);
     }
   }
 
