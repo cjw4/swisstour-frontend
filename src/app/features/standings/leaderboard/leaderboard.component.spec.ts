b7abf21ac2825166f3ad5b7c2f7ff2433cfc8c95
Christopher Walker <512444+cjw4@users.noreply.github.com>
2026-06-14 12:57:53 +0200

    test: fix tests to pass

--- src/app/features/standings/leaderboard/leaderboard.component.spec.ts
+++ src/app/features/standings/leaderboard/leaderboard.component.spec.ts
@@ -1,6 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { convertToParamMap, provideRouter } from '@angular/router';
+import { EventEmitter } from '@angular/core';
 
 import { LeaderboardComponent } from './leaderboard.component';
+import { ActivatedRoute } from '@angular/router';
+import { PlayersService } from '../../../api/services/players.service';
+import { StandingsService } from '../../../api/services/standings.service';
+import { TranslateService } from '@ngx-translate/core';
+
+const activatedRouteMock = {
+  paramMap: of(convertToParamMap({ year: '2024' }))
+};
+
+const playersServiceMock = {
+  getAllPlayers: jasmine.createSpy('getAllPlayers').and.returnValue(of([]))
+};
+
+const standingsServiceMock = {
+  getStandings: jasmine.createSpy('getStandings').and.returnValue(of([]))
+};
+
+const translateServiceMock = {
+  instant: jasmine.createSpy('instant').and.returnValue(''),
+  get: jasmine.createSpy('get').and.returnValue(of('')),
+  onLangChange: new EventEmitter(),
+  onTranslationChange: new EventEmitter(),
+  onDefaultLangChange: new EventEmitter()
+};
 
 describe('LeaderboardComponent', () => {
   let component: LeaderboardComponent;
@@ -8,7 +35,14 @@ describe('LeaderboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LeaderboardComponent]
+      imports: [LeaderboardComponent],
+      providers: [
+        provideRouter([]),
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+        { provide: PlayersService, useValue: playersServiceMock },
+        { provide: StandingsService, useValue: standingsServiceMock },
+        { provide: TranslateService, useValue: translateServiceMock }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LeaderboardComponent);
--- src/app/features/standings/leaders/leaders.component.spec.ts
+++ src/app/features/standings/leaders/leaders.component.spec.ts
@@ -1,17 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { convertToParamMap, provideRouter } from '@angular/router';
+import { EventEmitter } from '@angular/core';
 
-import { LeaderboardsComponent } from './leaders.component';
+import { LeadersComponent } from './leaders.component';
+import { ActivatedRoute } from '@angular/router';
+import { PlayersService } from '../../../api/services/players.service';
+import { StandingsService } from '../../../api/services/standings.service';
+import { TranslateService } from '@ngx-translate/core';
 
-describe('LeaderboardsComponent', () => {
-  let component: LeaderboardsComponent;
-  let fixture: ComponentFixture<LeaderboardsComponent>;
+const activatedRouteMock = {
+  paramMap: of(convertToParamMap({ year: '2024' }))
+};
+
+const playersServiceMock = {
+  getAllPlayers: jasmine.createSpy('getAllPlayers').and.returnValue(of([]))
+};
+
+const standingsServiceMock = {
+  getStandings: jasmine.createSpy('getStandings').and.returnValue(of([]))
+};
+
+const translateServiceMock = {
+  instant: jasmine.createSpy('instant').and.returnValue(''),
+  get: jasmine.createSpy('get').and.returnValue(of('')),
+  onLangChange: new EventEmitter(),
+  onTranslationChange: new EventEmitter(),
+  onDefaultLangChange: new EventEmitter()
+};
+
+describe('LeadersComponent', () => {
+  let component: LeadersComponent;
+  let fixture: ComponentFixture<LeadersComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LeaderboardsComponent]
+      imports: [LeadersComponent],
+      providers: [
+        provideRouter([]),
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+        { provide: PlayersService, useValue: playersServiceMock },
+        { provide: StandingsService, useValue: standingsServiceMock },
+        { provide: TranslateService, useValue: translateServiceMock }
+      ]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(LeaderboardsComponent);
+    fixture = TestBed.createComponent(LeadersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
